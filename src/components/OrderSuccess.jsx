53f54e871e6ea10ad2747e3eb61cb451db8a82d4
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-08-29 21:57:39 +0530

    style : width changed

--- src/components/OrderSuccess.jsx
+++ src/components/OrderSuccess.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const OrderSuccess = () => {
   return (
-    <div className='mt-[150px] my-auto mb-[150px]'>
+    <div className='mt-[150px] md:mt-12  mb-[150px]'>
       
     <p className='dark:text-brand-beige text-3xl text-center'>
     Order Placed 
     </p>
-    <img className='rounded-2xl mx-auto mt-4 w-[70%]' src="https://cdn.dribbble.com/users/3955123/screenshots/7107965/image.gif" alt="" />
+    <img className='rounded-2xl mx-auto mt-4 w-[70%] md:w-[30%] ' src="https://cdn.dribbble.com/users/3955123/screenshots/7107965/image.gif" alt="" />
       
     </div>
   )
