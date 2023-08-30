ec66dfeb63dba88a3b659106034fcab9baece66d
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-08-30 20:34:53 +0530

    feat: footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,11 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="w-full my-auto">
-      <p className='text-9xl font-sans font-bold text-gray-300  leading-[95px] md:text-center ' >LIVE FOR FOOD</p>
-      <br />
+      <p className='text-4xl font-sans font-semibold text-gray-300  leading-[95px] md:text-center ' >"यथा अन्नम् तथा मन्नम्।"</p>
+
+      <p className='text-2xl ml-2 font-sans  text-gray-300 md:text-center' >" as is the food, so is the mind "</p>
      
-      <p className='text-2xl ml-2 font-sans font-bold text-gray-300 md:text-center' >MADE BY FOOD LOVER</p>
-     
-      <p className='text-2xl mb-3 ml-2 font-sans font-bold text-gray-300 md:text-center' >EATSY HQ, MUNGER</p>
+      <p className='text-2xl mb-3 ml-2 font-sans text-gray-300 md:text-center' >Baked with bliss in India</p>
     </footer>
   )
 }
