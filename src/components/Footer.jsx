99c356187ad90dce6807bae705d22b020598aca5
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2023-08-30 20:38:38 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export function Footer() {
   return (
     <footer className="w-full my-auto">
-      <p className='text-4xl font-sans font-semibold text-gray-300  leading-[95px] md:text-center ' >"यथा अन्नम् तथा मन्नम्।"</p>
+      <p className='text-4xl font-sans font-semibold text-gray-300  leading-[95px] md:text-center ' >" यथा अन्नम् तथा मन्नम्। "</p>
 
       <p className='text-2xl ml-2 font-sans  text-gray-300 md:text-center' >" as is the food, so is the mind "</p>
      
